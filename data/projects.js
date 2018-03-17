f816205292183c2187d2e54db01fd69a977c8594
Mark Alaniz <markalaniz000@gmail.com>
2018-03-17 18:56:30 -0500

    created handlebars template for listing porjects, added datafolder to hold project info

---
+++ data/projects.js
@@ -0,0 +1,40 @@
+exports.projectList = [
+  {
+    name: "RangeFront",
+    link: "https://rangefront.herokuapp.com/",
+    description:
+      "In a team of 3, our goal is to take a merchant's listed item all the way to checkout as we manage the front-end, back-end, and database. RangeFront is a full-stack outdoors ecommerce webpage that does just that.",
+    image: "/images/rangefront.jpg"
+  },
+  {
+    name: "BitBay",
+    link: "https://digald.github.io/BitBay/",
+    description:
+      "Bitcoin had a huge peak in late 2017 which sparked the idea for a webpage. BitBay has the ability to track bitcoin statistics and information. Additionally, users may also search for their favorite products on ebay and compare the cost in USD to Bitcoin",
+    image: "/images/bitbay.png"
+  },
+  {
+    name: "RuneScrape",
+    link: "http://runescrape.herokuapp.com/",
+    description: "runescrape",
+    image: "/images/runescrape.png"
+  },
+  {
+    name: "Friend Finder",
+    link: "https://friendfinder235.herokuapp.com/",
+    description: "friendfinder",
+    image: "/images/friendfinder.png"
+  },
+  {
+    name: "Constructor Hangman",
+    link: "https://github.com/Digald/Constructor-Hangman",
+    description: "hangman",
+    image: "/images/constructor-hangman.png"
+  },
+  {
+    name: "Liri Bot",
+    link: "https://github.com/Digald/Liri-Node-App",
+    description: "liri",
+    image: "/images/liri.png"
+  }
+];
--- routes/view.js
+++ routes/view.js
@@ -1,17 +1,20 @@
+var projects = require("../data/projects.js");
+
 module.exports = function(app) {
   app.get("/", function(req, res) {
-      res.render('home', {
-        title: "Wecome to Mark's Portfolio",
-        css: "home.css",
-        js: "home.js"
-      });
+    res.render("home", {
+      title: "Wecome to Mark's Portfolio",
+      css: "home.css",
+      js: "home.js",
+      projects: projects.projectList
+    });
   });
 
-  app.get('/about', function(req,res){
-    res.render('about', {
-        title: 'About Mark',
-        css: 'about.css',
-        js: 'about.js'
+  app.get("/about", function(req, res) {
+    res.render("about", {
+      title: "About Mark",
+      css: "about.css",
+      js: "about.js"
     });
   });
 };
