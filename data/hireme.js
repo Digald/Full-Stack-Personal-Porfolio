9d670736d47ef63e83a6e617910ce27edb0657da
Mark Alaniz <markalaniz000@gmail.com>
2018-05-01 22:46:38 -0500

    chnaged colors on hire me page and then added new banner at the bottom to link to contact form

--- data/hireme.js
+++ data/hireme.js
@@ -3,11 +3,11 @@ exports.hireme = [
     id: 1,
     header: "WHY HIRE ME?",
     text:
-      "I am a web developer who consistantly practices the craft in my spare time. I continue to learn new things whether it takes minutes or weeks. "
+      "I am a web developer who consistantly practices the craft in my spare time. I continue to learn new things whether it takes minutes or weeks. I have the ability to research problems I have and take the necessary steps to solve them or work around them. I effectively try to communicate and understand the needs and requirements of others as well as express my own."
   },
   {
     id: 2,
-    header: "WHAT CAN I DO?",
-    text: "list my skills again"
+    header: "WHAT SERVICES CAN I PROVIDE?",
+    text: "TBD"
   }
 ];
