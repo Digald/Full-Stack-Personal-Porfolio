cec6a7743df2cea6b13bdd30a31dae00f3dc9b53
Mark Alaniz <markalaniz000@gmail.com>
2018-05-06 01:09:10 -0500

    Added jquery dynamic messages to help user determine if their message was recieved or not

--- public/js/about.js
+++ public/js/about.js
@@ -1,25 +1,46 @@
 $(document).ready(function() {
-    $('#contact-form').on('submit',function(e){
-        e.preventDefault();
-        var name = $("#nameInput").val().trim();
-        var email = $("#emailInput").val().trim();
-        var subject = $("#subjectInput").val().trim();
-        var message = $('#messageInput').val().trim();
-        $("#nameInput").val('');
-        $("#emailInput").val('');
-        $("#subjectInput").val('');
-        $('#messageInput').val('');
-        $.ajax({
-            method: 'POST',
-            url: '/sendmail',
-            data: {
-                name: name,
-                email: email,
-                subject: subject,
-                message: message
-            }
-        }).done(function(response){
-            console.log(response);
-        });
+  $("#contact-form").on("submit", function(e) {
+    e.preventDefault();
+    var name = $("#nameInput")
+      .val()
+      .trim();
+    var email = $("#emailInput")
+      .val()
+      .trim();
+    var subject = $("#subjectInput")
+      .val()
+      .trim();
+    var message = $("#messageInput")
+      .val()
+      .trim();
+    $("#nameInput").val("");
+    $("#emailInput").val("");
+    $("#subjectInput").val("");
+    $("#messageInput").val("");
+    $("#contact-form").hide();
+    $("#contact").append("<p class='message-1'>Sending Message...</p>");
+    $.ajax({
+      method: "POST",
+      url: "/sendmail",
+      data: {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+      }
+    }).done(function(res) {
+      console.log(res);
+      if (res.mailSent) {
+        $(".message-1").hide();
+        $("#contact").append(
+          "<p>Message sucessfully recieved! Thank you for your inquiry and I will get in touch as soon as I am available.</p>"
+        );
+      } else {
+        $(".message-1").hide();
+        $("#contact").append(
+          "<p>FAILED TO SEND :( Something might have broken so please try again later or try to reach me through my other social media links.</p>"
+        );
+      }
     });
+  });
 });
